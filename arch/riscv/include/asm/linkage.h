997f905249cfa3daa9c38261091c967b5ef623b5
xuyb1999 <xuyibin17@mails.ucas.ac.cn>
2023-09-27 09:25:39 +0800

    riscv,linkage: use 8 bytes alignment for RV64, 4 for RV32

--- arch/riscv/include/asm/linkage.h
+++ arch/riscv/include/asm/linkage.h
@@ -14,7 +14,12 @@
 #ifndef _ASM_RISCV_LINKAGE_H
 #define _ASM_RISCV_LINKAGE_H
 
+#ifdef CONFIG_64BIT
+#define __ALIGN     .balign 8
+#define __ALIGN_STR ".balign 8"
+#else
 #define __ALIGN		.balign 4
 #define __ALIGN_STR	".balign 4"
+#endif /* CONFIG_64BIT */
 
 #endif /* _ASM_RISCV_LINKAGE_H */
