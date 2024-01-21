96e1f6538a02901c03a63b19c6328b27e4239135
wanghan192 <wanghan192@mails.ucas.ac.cn>
2024-01-21 22:15:07 +0800

    [upgrade] Put vdso in trust area when dynamic

--- arch/riscv/kernel/vdso.c
+++ arch/riscv/kernel/vdso.c
@@ -17,6 +17,7 @@
 #else
 #include <asm/vdso.h>
 #endif
+#include <asm/kdasics.h>
 
 extern char vdso_start[], vdso_end[];
 
@@ -68,7 +69,20 @@ int arch_setup_additional_pages(struct linux_binprm *bprm,
 	if (mmap_write_lock_killable(mm))
 		return -EINTR;
 
+
+#ifdef CONFIG_DASICS
+	if (unlikely(current->dasics_state == DASICS_DYNAMIC))
+	{
+		vdso_base = DASICS_VDSO_BASE;
+	} else 
+	{
+		vdso_base = get_unmapped_area(NULL, 0, vdso_len, 0, 0);
+	}	
+#else
 	vdso_base = get_unmapped_area(NULL, 0, vdso_len, 0, 0);
+#endif
+
+
 	if (IS_ERR_VALUE(vdso_base)) {
 		ret = vdso_base;
 		goto end;
