b7baad99e0c4bfc3065dc5d9f5ad7b153553e0fd
wanghan192 <wanghan192@mails.ucas.ac.cn>
2024-04-01 16:21:55 +0800

    (init): nest dasics init

--- arch/riscv/include/asm/ptrace.h
+++ arch/riscv/include/asm/ptrace.h
@@ -80,10 +80,16 @@ struct pt_regs {
     unsigned long dasicsLibCfg1;    // reserved
     unsigned long dasicsLibBounds[16][2];
     unsigned long dasicsMaincall;
-    unsigned long dasicsReturnPC;
+    unsigned long dasicsReturnPC[4];
     unsigned long dasicsFreezoneRet;
     unsigned long dasicsJumpBounds[4][2];
     unsigned long dasicsJumpCfg;
+	unsigned long dasicsScratchCfg;
+	unsigned long dasicsScratchBoundLo;
+	unsigned long dasicsScratchBoundHi;
+	unsigned long dasicsMemLevel;
+	unsigned long dasicsJmpLevel;
+
 #endif
 
 #ifdef CONFIG_RISCV_MEMORY_PROTECTION_KEYS
